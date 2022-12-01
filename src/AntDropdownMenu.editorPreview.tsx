2c62933a8afe7186a2b43a800910d80e27edfbbb
wiwengweng <ylyy-1985@163.com>
2022-12-01 15:43:13 +0800

    delete lock file

--- src/AntDropdownMenu.editorPreview.tsx
+++ src/AntDropdownMenu.editorPreview.tsx
@@ -7,20 +7,23 @@ import { AntDropdownMenuPreviewProps } from "../typings/AntDropdownMenuProps";
 export class preview extends Component<AntDropdownMenuPreviewProps> {
     render(): ReactNode {
         // return <HelloWorldSample sampleText={""} />;
-        if (this.props.renderType === 'dropdown') {
-            return <Dropdown
-                placement={this.props.placement}
-                destroyPopupOnHide={this.props.destroyPopupOnHide}
-                arrow={this.props.arrow}
-                autoFocus={this.props.autoFocus}
-                className={this.props.class}
-                trigger={[this.props.trigger]}>
-                <Space>
-                    {this.props.label}
-                    <DownOutlined />
-                </Space>
-            </Dropdown>
-        } else if (this.props.renderType === 'dropdownButton') {
+        if (this.props.renderType === "dropdown") {
+            return (
+                <Dropdown
+                    placement={this.props.placement}
+                    destroyPopupOnHide={this.props.destroyPopupOnHide}
+                    arrow={this.props.arrow}
+                    autoFocus={this.props.autoFocus}
+                    className={this.props.class}
+                    trigger={[this.props.trigger]}
+                >
+                    <Space>
+                        {this.props.label}
+                        <DownOutlined />
+                    </Space>
+                </Dropdown>
+            );
+        } else if (this.props.renderType === "dropdownButton") {
             return (
                 <Dropdown.Button
                     placement={this.props.placement}
@@ -29,12 +32,11 @@ export class preview extends Component<AntDropdownMenuPreviewProps> {
                     autoFocus={this.props.autoFocus}
                     className={this.props.class}
                     trigger={[this.props.trigger]}
-                    icon={<DownOutlined />}>
-                    <Space>
-                        {this.props.label}
-                    </Space>
+                    icon={<DownOutlined />}
+                >
+                    <Space>{this.props.label}</Space>
                 </Dropdown.Button>
-            )
+            );
         }
     }
 }
